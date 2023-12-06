c2339fa82f0f802f54c18283251fefb52d049a20
bielFernandes <gabrielfdias082@gmail.com>
2023-12-06 00:06:47 -0300

    fix: set uid with user email

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  private
+  def set_uid
+    self[:uid] = self[:email] if self[:uid].blank? && self[:email].present?
+  end
 end
