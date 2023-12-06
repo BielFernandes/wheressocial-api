6d6fe644c478aa3041fd53de06e5d07070f11134
bielFernandes <gabrielfdias082@gmail.com>
2023-12-05 22:03:33 -0300

    regenerate Post resources

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,3 +1,3 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :content, :text
+  attributes :id, :content
 end
--- db/migrate/20231206005136_create_posts.rb
+++ db/migrate/20231206005136_create_posts.rb
@@ -1,8 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.string :content
-      t.string :text
+      t.text :content, null: false
 
       t.timestamps
     end
