34132e7d25f0173d59e53a7e6818319d8e0eac42
bielFernandes <gabrielfdias082@gmail.com>
2023-12-07 17:44:32 -0300

    aninhed posts e comments routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :comments
   mount_devise_token_auth_for 'User', at: 'auth'
-  resources :posts
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :posts do
+    resources :comments
+  end
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
