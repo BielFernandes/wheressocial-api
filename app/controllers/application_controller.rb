cb9bd0867c0ed159bf3041c10d2f9f34e102b56a
bielFernandes <gabrielfdias082@gmail.com>
2023-12-07 17:41:16 -0300

    set required prop of user in devise token auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::API
         include DeviseTokenAuth::Concerns::SetUserByToken
+        before_action :configure_permitted_parameters, if: :devise_controller?
+
         def render404
                 render json: { error: 'Not Found' }, status: :not_found
+        end
+
+        protected
+
+        def configure_permitted_parameters
+          devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
         end
 end
