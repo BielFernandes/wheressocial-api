6feea0363f08cee3decf03da3182c752396a446b
bielFernandes <gabrielfdias082@gmail.com>
2023-12-14 22:03:36 -0300

    delete: validate req body in AppController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,15 +6,7 @@ class ApplicationController < ActionController::API
         def render404
                 render json: { error: 'Not Found' }, status: :not_found
         end
-
-        private
-        def check_empty_request_body
-                if request.body.read.blank?
-                  puts 'THE REQUEST BODY IS EMPTY.'
-                  render json: { error: "don't send data." }, status: :unprocessable_entity
-                end
-        end
-
+        
         protected
         def configure_permitted_parameters
           devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
