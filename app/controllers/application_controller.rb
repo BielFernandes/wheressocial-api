f42aac73273a7f731bf24dc2ab0491b89e49181e
bielFernandes <gabrielfdias082@gmail.com>
2023-12-14 23:09:07 -0300

    fix: temporary comment aplication method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,21 @@
 class ApplicationController < ActionController::API
         include DeviseTokenAuth::Concerns::SetUserByToken
         before_action :configure_permitted_parameters, if: :devise_controller?
-        before_action :check_empty_request_body
+        # before_action :check_empty_request_body
 
         def render404
                 render json: { error: 'Not Found' }, status: :not_found
         end
 
+        # private
+        # def check_empty_request_body
+        #         if request.body.read.blank?
+        #           puts 'THE REQUEST BODY IS EMPTY.'
+        #           render json: { error: "don't send data." }, status: :unprocessable_entity
+        #         end
+        # end
+
         protected
-
-        def check_empty_request_body
-                if request.body.read.blank?
-                  puts 'THE REQUEST BODY IS EMPTY(ApplicationController)'.red
-                  render json: { error: "don't sent datas" }, status: :unprocessable_entity
-                end
-        end
-
         def configure_permitted_parameters
           devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
         end
