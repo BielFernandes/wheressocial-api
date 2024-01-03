47c7591ab34efbdf205298da5691d5ca7d8b583c
BielFernandes <gabrielfdias082@gmail.com>
2024-01-02 21:06:13 -0300

    test: context show method of posts controller

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -6,11 +6,14 @@ RSpec.describe Api::V1::PostsController, :type => :controller do
   before(:each) do
     @user = create(:user)
     @auth_headers = @user.create_new_auth_token
+    request.headers.merge!(@auth_headers)
   end
+
+  let(:user)  { create(:user) }
+  let(:post) { create(:post, user_id: user.id) }
 
   context "GET #index" do
     it "should success and render all posts json" do
-      request.headers.merge!(@auth_headers)
       get :index
       expect(response).to have_http_status(200)
       expect(response.content_type).to include('application/json')
@@ -23,15 +26,29 @@ RSpec.describe Api::V1::PostsController, :type => :controller do
     end
 
     it "should have one post" do
-      user = create(:user)
-      post = create(:post, user_id: user.id)
       get :index
       json_response = JSON.parse(response.body)
       expect(json_response).not_to be_nil
     end
   end
 
-  context "GET @show" do
-    
+  context "GET #show" do
+    it "should return the post details" do
+      get :show, params: { id: post.id }
+      expect(response). to have_http_status(:success)
+      json_response = JSON.parse(response.body)
+    end
+
+    it "should return the correct post according to the parameter" do
+      get :show, params: { id: post.id }
+      json_response = JSON.parse(response.body)
+      expect(json_response['id']).to eq(post.id)
+    end
+
+    it "should return 'not found' for a nonexistent post" do
+      get :show, params: { id: 999 }
+      json_response = JSON.parse(response.body)
+      expect(response).to have_http_status(:not_found)
+    end
   end
 end
